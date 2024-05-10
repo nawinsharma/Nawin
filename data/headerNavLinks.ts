8d0bd9730b4dc934d040e30217b6fe0a1893038c
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-05-10 23:44:48 +0530

    :sparkles: data for navlink

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+export let headerNavLinks = [
+    { href: '/', title: 'Home' },
+    { href: '/about', title: 'About Me' },
+    { href: '/skills', title: 'skills' },
+    { href: '/projects', title: 'Projects' },
+    { href: '/resume', title: 'Resume' },
+    { href: '/contact', title: 'Contact Me' },
+
+  ]

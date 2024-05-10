9e1f78676c3ad2ac4601f87c102c6c071aab2437
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-05-11 02:03:47 +0530

    :sparkles: Add custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Custom404() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <Image
+                src="/static/images/404.png"
+                alt="404"
+                height={800}
+                width={800}
+                className="flex justify-center content-center"
+            />
+        </div>
+    )
+}

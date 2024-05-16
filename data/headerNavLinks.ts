f8efbc80eaaf5d484bf6550c3cc43b3034c1607e
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-05-17 00:36:04 +0530

    fix:home page

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 export let headerNavLinks = [
-    { href: '/', title: 'Home' },
-    { href: '#about', title: 'About Me' },
-    { href: '#skills', title: 'Skills' },
-    { href: '#projects', title: 'Projects' },
-    { href: '#contact', title: 'Contact Me' },
-  ]
+  { href: '/', title: 'Home' },
+  // { href: '#about', title: 'About Me' },
+  // { href: '#skills', title: 'Skills' },
+  { href: '#projects', title: 'Projects' },
+  // { href: '#contact', title: 'Contact Me' },
+]
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import { Spotlight } from "../../../components/ui/SpotLight";
 import clsx from "clsx";
 
@@ -8,12 +8,12 @@ export function HeroSection() {
     'mb-8 bg-clip-text text-4xl font-extrabold leading-[60px] tracking-tight text-transparent md:text-7xl md:leading-[86px]'
   )
   return (
-    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="h-screen w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden ">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
-      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+      <div className=" p-4 max-w-7xl mx-auto relative z-10  w-full pt-20 md:pt-0">
         <h1 className="text-4xl md:text-7xl font-bold  bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           <div className={className}>
             Hello, there!<i className="twa twa-waving-hand"></i>
@@ -22,12 +22,12 @@ export function HeroSection() {
           </div>
         </h1>
         <div className="flex items-center gap-5">
-          <span className="text-xl sm:text-3xl md:text-5xl font-bold text-white">
+          <span className="text-xl sm:text-3xl md:text-7xl text-center font-sans font-bold mb-[17px]8 text-white">
             I&apos;m Nawin
           </span>
         </div>
-        <div className="mt-9 text-gray-300">
-          A passionate Developer 🚀 having an experience of building Full Stack Web applications with <br /> JavaScript / TypeScript / Reactjs / Next js / Nodejs / Express / Graphql /  Hono   and some other cool libraries and frameworks.
+        <div className="mt-9 w-full flex text-gray-300  text-[20px] max-w-3xl leading-[30px]">
+          A passionate Developer 🚀 having an experience of building Full Stack Web applications with JavaScript / TypeScript / Reactjs / Next js / Nodejs / Express / Graphql /  Hono   and some other cool libraries and frameworks.
         </div>
       </div>
     </div>

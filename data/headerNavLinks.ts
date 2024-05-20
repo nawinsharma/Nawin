cf06a1c0656f14629b276f73f0c1b2fde206fd55
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-05-20 21:40:19 +0530

    add contact in navlink

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,5 +3,5 @@ export let headerNavLinks = [
   // { href: '#about', title: 'About Me' },
   { href: '#skills', title: 'Skills' },
   { href: '#projects', title: 'Projects' },
-  // { href: '#contact', title: 'Contact Me' },
+  { href: '#contact', title: 'Contact Me' },
 ]

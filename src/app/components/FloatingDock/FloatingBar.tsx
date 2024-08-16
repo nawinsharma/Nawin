8d416c17d5aca4e1f74c9a2e908b4d2531af0f5d
Nawin <nawinsharma60@gmail.com>
2024-08-16 23:57:16 +0530

    :sparkles: Add a fixed floating bar for easy navigation

---
+++ src/app/components/FloatingDock/FloatingBar.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { FloatingDock } from "../../../../components/ui/floating-dock";
+import {
+  IconHome,
+  IconShield,
+  IconBrandProducthunt,
+  IconMessage,
+} from "@tabler/icons-react";
+
+export function FloatingBar() {
+  const links = [
+    {
+      title: "Home",
+      icon: (
+        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Skills",
+      icon: (
+        <IconShield className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#skills",
+    },
+    {
+      title: "Projects",
+      icon: (
+        <IconBrandProducthunt className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#projects",
+    },
+    {
+      title: "Contact",
+      icon: (
+        <IconMessage className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#contact",
+    },
+  ];
+
+  return (
+    <div className="fixed bottom-0 left-0 right-0 z-50 flex justify-center py-4 shadow-lg">
+      <FloatingDock
+        mobileClassName="translate-y-2" // only for demo, remove for production
+        items={links}
+      />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,22 @@ import { HeroSection } from "./components/Hero";
 import { Projects } from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
+import { FloatingBar } from "./components/FloatingDock/FloatingBar"; // Adjust the path as necessary
 
 export default function Home() {
   return (
-    <main className="relative flex justify-center overflow-x-hidden items-center flex-col mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <LayoutWrapper>
-          <HeroSection />
-          <Skills />
-          <Projects />
-          <Contact />
-        </LayoutWrapper>
-      </div>
-    </main>
+    <>
+      <main className="relative flex justify-center overflow-x-hidden items-center flex-col mx-auto sm:px-10 px-5">
+        <div className="max-w-7xl w-full">
+          <LayoutWrapper>
+            <HeroSection />
+            <Skills />
+            <Projects />
+            <Contact />
+          </LayoutWrapper>
+        </div>
+      </main>
+      <FloatingBar />
+    </>
   );
 }
